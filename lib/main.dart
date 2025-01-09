fafdcaeb50bd252ca03372c3d15f9406397330c1
mistralll <me.pooh441@gmail.com>
2025-01-09 19:39:30 +0900

    feat: read .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_playground/screens/search_screen.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
