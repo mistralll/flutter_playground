3cca411abeb8154265226c4b2d702f8f5a495f08
mistralll <me.pooh441@gmail.com>
2025-01-09 19:22:50 +0900

    feat: search screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_playground/screens/search_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Qiita Search',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        fontFamily: 'Hiragino Sans',
+        appBarTheme: const AppBarTheme(backgroundColor: Color(0xFF55C500)),
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Colors.white,
+            ),
+      ),
+      home: const SearchScreen(),
     );
   }
 }
