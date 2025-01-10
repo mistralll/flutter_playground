ec9c956cc69edbd3ef06a84e255ecad7b8c2dcef
mistralll <me.pooh441@gmail.com>
2025-01-10 23:21:48 +0900

    change: move search function

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_playground/models/article.dart';
 import 'package:flutter_playground/screens/search_screen.dart';
 
 Future<void> main() async {
@@ -28,24 +24,5 @@ class MyApp extends StatelessWidget {
       ),
       home: const SearchScreen(),
     );
-  }
-
-  Future<List<Article>> searchQiita(String keyword) async {
-    final uri = Uri.https('qiita.com', '/api/v2/items', {
-      'query': 'title:$keyword',
-      'per_page': '10',
-    });
-
-    final String token = dotenv.env['QIITA_ACCESS_TOKEN'] ?? '';
-
-    final http.Response res = await http.get(uri, headers: {
-      'Authorization': 'Bearer $token',
-    });
-    if (res.statusCode == 200) {
-      final List<dynamic> body = jsonDecode(res.body);
-      return body.map((dynamic json) => Article.fromJson(json)).toList();
-    } else {
-      return [];
-    }
   }
 }
--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:flutter_playground/models/article.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -8,6 +13,25 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  Future<List<Article>> searchQiita(String keyword) async {
+    final uri = Uri.https('qiita.com', '/api/v2/items', {
+      'query': 'title:$keyword',
+      'per_page': '10',
+    });
+
+    final String token = dotenv.env['QIITA_ACCESS_TOKEN'] ?? '';
+
+    final http.Response res = await http.get(uri, headers: {
+      'Authorization': 'Bearer $token',
+    });
+    if (res.statusCode == 200) {
+      final List<dynamic> body = jsonDecode(res.body);
+      return body.map((dynamic json) => Article.fromJson(json)).toList();
+    } else {
+      return [];
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
